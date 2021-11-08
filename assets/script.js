89df36c93fef57c5766d1a0cacbc7afa30d5fd16
Ddorto1 <dallasdorton@yahoo.com>
2021-11-07 17:33:22 -0700

    add password prompts

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    window.prompt ("How many characters, from 8 to 128, would you like your password to contain? ");
+    window.confirm ("Would you like your password to contain upper case letters?");
+    window.confirm ("Would you like your password to contain lower case letters?");
+    window.confirm ("Would you like your password to contain special characterrs?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
